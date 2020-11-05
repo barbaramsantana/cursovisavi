08553b19dc6d32033d76107f70521b61f24bf5f9
EikESousA <eike.sousa@hotmail.com>
2020-11-05 11:52:02 -0300

    eslint

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
 
 import indexImage from '../../assets/index.png';
@@ -18,11 +17,11 @@ const Index: React.FC = () => {
   return (
     <Container>
       <Title>Encontre mapas como os dados do Covid-19 em Sergipe </Title>
-        <Link to="/se">
-      <ContainerImage>
+      <Link to="/se">
+        <ContainerImage>
           <Image src={indexImage} alt="Logo de SE" />
-      </ContainerImage>
-        </Link>
+        </ContainerImage>
+      </Link>
       <ContainerButton>
         <Link to="/se/page1">
           Visualizar Mapas
