93045e19aa6ad5f48e6832f6296fe23491f7d3ce
Bárbara Mota Santana <barbaramotabms@gmail.com>
2020-12-21 09:11:08 -0300

    variavel de ambiente

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'https://backendvisavi.herokuapp.com/',
-  baseURL: 'https://visavi-server-2.herokuapp.com/',
+  //baseURL: 'https://visavi-server-2.herokuapp.com/',
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
